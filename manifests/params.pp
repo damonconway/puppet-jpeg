7b380402641abf87077bad6959db0b8adcb5ceae
Damon Conway <damon.conway@alchemysystems.com>
2014-11-06 13:16:44 -0600

    Passing syntax checks.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,20 +5,20 @@ class jpeg::params () {
     'RedHat': {
       case $::lsbmajdistrelease {
         '5': {
-          package_name     = 'libjpeg'
-          dev_package_name = 'libjpeg-devel'
+          $package_name     = 'libjpeg'
+          $dev_package_name = 'libjpeg-devel'
         }
     
         default: {
-          package_name     = 'libjpeg-turbo'
-          dev_package_name = 'libjpeg-turbo-devel'
+          $package_name     = 'libjpeg-turbo'
+          $dev_package_name = 'libjpeg-turbo-devel'
         }
       }
     }
 
     'Debian': {
-       package_name     = 'libjpeg-turbo8'
-       dev_package_name = 'libjpeg-turbo-dev'
+       $package_name     = 'libjpeg-turbo8'
+       $dev_package_name = 'libjpeg-turbo-dev'
     }
   }
 
